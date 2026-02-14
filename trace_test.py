eac2e70458267f6cfa9bb8b8826b279e0ba172e4
Milo <milovan_jovic@student.uml.edu>
2026-02-13 19:07:21 -0500

    modified trace test patttern

--- trace_test.py
+++ trace_test.py
@@ -4,54 +4,111 @@ from motor_control import (move_linear_stage)
 from relay_control import (nordson_on, nordson_off)
 
 #parameters for line test
-frd = '+'
-bck = '-'
 
 x = str('X')
 y = str('Y')
 z = str('Z')
 
-l = 2000.0 #Trace length in steps 
+l = 3000.0 #Trace length in steps 
 tapl = 2000.0
 stp = 1000.0
+delay = 2
 
+def up(length):
+    move_linear_stage(z, '+', length, wait_for_stop=True, max_wait=30.0)
+    
+
+def down(length):
+    move_linear_stage(z, '-', length, wait_for_stop=True, max_wait=30.0)
+    
+
+def left(length):
+    move_linear_stage(x, '+', length, wait_for_stop=True, max_wait=30.0)
+    
+
+def right(length):
+    move_linear_stage(x, '-', length, wait_for_stop=True, max_wait=30.0)
+    
+
+def front(length):
+    move_linear_stage(y, '-', length, wait_for_stop=True, max_wait=30.0)
+    
+
+def back(length):
+    move_linear_stage(y, '+', length, wait_for_stop=True, max_wait=30.0)
+    
 
 def tap():
-    move_linear_stage(y, frd, tapl,wait_for_completion=True)
-    time.sleep(2)
-    move_linear_stage(y, bck, tapl,wait_for_completion=True)
-    #tap
+    down(tapl)
+    up(tapl)
 
-def print_trace():
-    nordson_on()
-    time.sleep(2)
-    move_linear_stage(x, bck, l, wait_for_completion=True)
-    nordson_off()
-    time.sleep(2)
-
-def side_step():
-    move_linear_stage(x, frd, stp, True)
-    time.sleep(2)
+def line_test_2():
+    left(5000.0)
 
 def line_test_1():
     """Test moving the linear stage forward and backward."""
     print("Starting line test 1...")
     
-    time.sleep(3)
-    move_linear_stage(z, '-', tapl, wait_for_stop=True, max_wait=30.0)
-    time.sleep(2)
-    move_linear_stage(z, '+', tapl, wait_for_stop=True, max_wait=30.0)
-    time.sleep(2)
-    #tap
-    
+    tap()
     nordson_on()
-    time.sleep(2)
-    move_linear_stage(y, '-', l, wait_for_stop=True, max_wait=30.0)
+    time.sleep(delay)
+    front(l)
     nordson_off()
-    time.sleep(2)
-    
-    move_linear_stage(x, '+', stp, wait_for_stop=True, max_wait=30.0)
-    time.sleep(2)
+    time.sleep(delay)
+
+    down(tapl)
+    back(l)
+    time.sleep(delay)
+    left(stp)
+    up(tapl)
+
+    nordson_on()
+    time.sleep(delay)
+    front(l)
+    nordson_off()
+    time.sleep(delay)
+
+    down(tapl)
+    back(l)
+    time.sleep(delay)
+    left(stp)
+    up(tapl)
+
+    nordson_on()
+    time.sleep(delay)
+    front(l)
+    nordson_off()
+    time.sleep(delay)
+
+    down(tapl)
+    back(l)
+    time.sleep(delay)
+    left(stp)
+    up(tapl)
+
+    nordson_on()
+    time.sleep(delay)
+    front(l)
+    nordson_off()
+    time.sleep(delay)
+
+    down(tapl)
+    back(l)
+    time.sleep(delay)
+    left(stp)
+    up(tapl)
+
+    nordson_on()
+    time.sleep(delay)
+    front(l)
+    nordson_off()
+    time.sleep(delay)
+
+    down(tapl)
+    back(l)
+    time.sleep(delay)
+    left(stp)
+    up(tapl)
 
     print("Line test 1 complete.")
     
