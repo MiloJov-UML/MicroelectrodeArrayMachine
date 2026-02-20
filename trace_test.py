8a2355dc34d63f5999c24205aceb797b5fdcf974
Milo <milovan_jovic@student.uml.edu>
2026-02-20 11:23:42 -0500

    added for loop to trace test

--- trace_test.py
+++ trace_test.py
@@ -46,69 +46,26 @@ def line_test_2():
     left(5000.0)
 
 def line_test_1():
+    
     """Test moving the linear stage forward and backward."""
+    
     print("Starting line test 1...")
     
     tap()
-    nordson_on()
-    time.sleep(delay)
-    front(l)
-    nordson_off()
-    time.sleep(delay)
+    
+    for i in range(1, 8, 1):
+        nordson_on()
+        time.sleep(delay)
+        front(l)
+        nordson_off()
+        time.sleep(delay)
 
-    down(tapl)
-    back(l)
-    time.sleep(delay)
-    left(stp)
-    up(tapl)
-
-    nordson_on()
-    time.sleep(delay)
-    front(l)
-    nordson_off()
-    time.sleep(delay)
-
-    down(tapl)
-    back(l)
-    time.sleep(delay)
-    left(stp)
-    up(tapl)
-
-    nordson_on()
-    time.sleep(delay)
-    front(l)
-    nordson_off()
-    time.sleep(delay)
-
-    down(tapl)
-    back(l)
-    time.sleep(delay)
-    left(stp)
-    up(tapl)
-
-    nordson_on()
-    time.sleep(delay)
-    front(l)
-    nordson_off()
-    time.sleep(delay)
-
-    down(tapl)
-    back(l)
-    time.sleep(delay)
-    left(stp)
-    up(tapl)
-
-    nordson_on()
-    time.sleep(delay)
-    front(l)
-    nordson_off()
-    time.sleep(delay)
-
-    down(tapl)
-    back(l)
-    time.sleep(delay)
-    left(stp)
-    up(tapl)
-
+        down(tapl)
+        back(l)
+        time.sleep(delay)
+        left(stp)
+        up(tapl)
+    
+    
     print("Line test 1 complete.")
     
